b5047f56252930d33ddd006825f561710517c825
Awais Ibrahim <maharawais348@gmail.com>
2020-11-26 11:10:31 +0500

    image processing

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,9 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+# bmi_calculator
 
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-  @@ -18,16 +18,33 @@ class _InputPageState extends State<InputPage> {
-  Expanded(child: Row(
-  children: <Widget>[
-  Expanded(
-  child: new RepeatContainerCode(colors: Color(0xFF1D1E33))
-  child: new RepeatContainerCode(colors: Color(0xFF1D1E33),
-  cardWidget: Column(
-  mainAxisAlignment: MainAxisAlignment.center,
-  children: <Widget>[
-  Icon(
-  FontAwesomeIcons.male,
-  size:80.0,
-  ),
-  SizedBox(
-  height: 15.0,
-  ),
-  Text(
-  'Male',
-  style: TextStyle(fontSize: 18.0,color: Color(0xFF8D8E98)),
-  ),
-  ],
-  ),
-  )
-  ),
-  Expanded(child: new RepeatContainerCode(colors: Colors.blueGrey,)),
-  Expanded(child: new RepeatContainerCode(colors: Color(0xFF1D1E33),)),
-  ],
-  )),
-  Expanded(child: new RepeatContainerCode(colors: Colors.greenAccent,)),
-  Expanded(child: new RepeatContainerCode(colors: Color(0xFF1D1E33),)),
-  Expanded(child: Row(
-  children: <Widget>[
-  Expanded(child: new RepeatContainerCode(colors: Colors.blueAccent,)),
-  Expanded(child: new RepeatContainerCode(colors: Colors.amberAccent,)),
-  Expanded(child: new RepeatContainerCode(colors: Color(0xFF1D1E33),)),
-  Expanded(child: new RepeatContainerCode(colors: Color(0xFF1D1E33),)),
-  ],
-  )),
-  ],
-  @@ -38,12 +55,14 @@ class _InputPageState extends State<InputPage> {
-  }
+A new Flutter application.
+![Screenshot_20201125-205503](https://user-images.githubusercontent.com/55029461/100260871-c949df80-2f6b-11eb-89fe-00ae7990f79c.png)
+![Screenshot_20201125-220031](https://user-images.githubusercontent.com/55029461/100260877-cb13a300-2f6b-11eb-80c3-01715adcc5c5.png)
+![Screenshot_20201125-220040](https://user-images.githubusercontent.com/55029461/100260878-cb13a300-2f6b-11eb-82fa-e857c7f01504.png)
+![Screenshot_20201125-220056](https://user-images.githubusercontent.com/55029461/100260885-cbac3980-2f6b-11eb-9a06-df85a63f3d09.png).
 
-  class RepeatContainerCode extends StatelessWidget {
-  RepeatContainerCode({@required this.colors});
-  RepeatContainerCode({@required this.colors,this.cardWidget});
-  final Color colors;
-  final cardWidget;
-  @override
-  Widget build(BuildContext context) {
-  return Container(
-  margin: EdgeInsets.all(15.0),
-  child: cardWidget,
-  decoration: BoxDecoration(
-  color:colors,
-  borderRadius: BorderRadius.circular(10.0)
-  7  pubspec.lock
-  @@ -67,6 +67,13 @@ packages:
-  description: flutter
-  source: sdk
-  version: "0.0.0"
-  font_awesome_flutter:
-  dependency: "direct main"
-  description:
-  name: font_awesome_flutter
-  url: "https://pub.dartlang.org"
-  source: hosted
-  version: "8.10.0"
-  matcher:
-  dependency: transitive
-  description:
-  1  pubspec.yaml
-  @@ -28,6 +28,7 @@ dependencies:
-  # The following adds the Cupertino Icons font to your application.
-  # Use with the CupertinoIcons class for iOS style icons.
-  cupertino_icons: ^1.0.0
-  font_awesome_flutter: ^8.10.0
-
-  dev_dependencies:
-  flutter_test:
+## Getting Started
